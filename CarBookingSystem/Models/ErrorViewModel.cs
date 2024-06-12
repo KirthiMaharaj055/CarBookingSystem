d8d8a4a2cd433ba7dd0845a76d55289c99c53f53
KirthiMaharaj055 <kirthimaharaj5@gmail.com>
2024-06-12 15:44:18 +0200

    Create project for car booking

---
+++ CarBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarBookingSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
