f93d8e80f91a1b58f36079d7c46c70e46fb71d75
KirthiMaharaj055 <kirthimaharaj5@gmail.com>
2024-06-14 15:09:29 +0200

    created view model class bookingadd

--- CarBookingSystem/obj/Debug/net8.0/CarBookingSystem.AssemblyInfo.cs
+++ CarBookingSystem/obj/Debug/net8.0/CarBookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f43c1e481ed530e9718deed588946772d7df92b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d91b35ac55f02f5bf046623d2a2f8bc01eb8f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
