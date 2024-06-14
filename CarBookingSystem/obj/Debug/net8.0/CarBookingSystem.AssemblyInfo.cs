2f43c1e481ed530e9718deed588946772d7df92b
KirthiMaharaj055 <kirthimaharaj5@gmail.com>
2024-06-14 15:07:29 +0200

    created view model class bookinglist

--- CarBookingSystem/obj/Debug/net8.0/CarBookingSystem.AssemblyInfo.cs
+++ CarBookingSystem/obj/Debug/net8.0/CarBookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0087234cc3cf96620c1f30c993f77a3f9247f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb57dd5dfdf27c1ef017020a9811251ccc8e119")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
