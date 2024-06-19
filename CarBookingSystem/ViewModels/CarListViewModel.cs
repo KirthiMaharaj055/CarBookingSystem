fe9838bb2b671f8f44aab5eee1a1aa88ca155918
KirthiMaharaj055 <kirthimaharaj5@gmail.com>
2024-06-19 12:43:43 +0200

    Add the delete function for book controller

--- CarBookingSystem/ViewModels/CarListViewModel.cs
+++ CarBookingSystem/ViewModels/CarListViewModel.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarBookingSystem.Models;
 
 namespace CarBookingSystem.ViewModels
 {
     public class CarListViewModel
     {
-        
+        public IEnumerable<Car> Cars { get; set; }
     }
-}
+}   
